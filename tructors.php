19ea6f3cc24ba424ef6095795109adb79ed98184
Muhammad Usman <usmansaleem4446996@gmail.com>
2024-06-24 16:08:42 +0500

    tructors

---
+++ tructors.php
@@ -0,0 +1,23 @@
+<?php
+//constructors and destructors
+
+class Laptop
+{
+    // __construct is the first pre-defined function which is executed when object is initialized
+    function __construct($ram)
+    {
+        //we can initialize var in __construct too instead of Access Modifiers
+        $this->ram = $ram;
+    }
+
+    // __destruct is the last pre-defined function which is executed when object is initialized
+    function __destruct()
+    {
+        echo "Shutting down";
+    }
+}
+
+//we can pass arg while initializing class for __construct
+$dell = new Laptop(8);
+
+echo $dell->ram;
