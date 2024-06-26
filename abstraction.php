a6b1733cf9478e0b3fcc83184e13f1ded91102b5
Muhammad Usman <usmansaleem4446996@gmail.com>
2024-06-26 12:10:43 +0500

    abstraction

---
+++ abstraction.php
@@ -0,0 +1,28 @@
+<?php
+//data abstraction is used to make compulsary function
+abstract class Orders
+{
+    //abstract class must have a abstract class which is only declared and is further defined by child class
+    abstract function setOrderId();
+}
+
+//child class inheriting abstract class must define abstruct function
+class Order extends Orders
+{
+    public $orderId;
+
+    //abstract function must be defined before using other functions
+    function setOrderId()
+    {
+        $this->orderId = rand(1, 10);
+    }
+
+    function getOrderId()
+    {
+        echo $this->orderId;
+    }
+}
+
+$order = new Order;
+$order->setOrderId();
+$order->getOrderId();
